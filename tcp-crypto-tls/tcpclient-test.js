e378791ce1fcaad86de6f8a439dde371a641f189
nemusika <nemusika@yo.rim.or.jp>
2024-11-28 05:54:28 +0900

    Update tcpclient-test.js

--- tcp-crypto-tls/tcpclient-test.js
+++ tcp-crypto-tls/tcpclient-test.js
@@ -1,9 +1,14 @@
 import { TcpClient } from "k6/x/tcpclient";
 
 export default function () {
-  let client = new TcpClient();
-  let conn = client.connect(true, "example.com:443", true);
-  conn.write("Hello\n");
-  let response = conn.readLine();
-  console.log(response);
+    let client = new TcpClient();
+    let conn = client.connect(true, "example.com:443", true);
+    
+    if (conn) {
+        conn.write("Hello\n");
+        let response = conn.readLine();
+        console.log(response);
+    } else {
+        console.error("接続に失敗しました");
+    }
 }
