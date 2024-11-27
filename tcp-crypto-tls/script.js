0873a2b6935b81a7cebdeec1db3df1b8d766e47a
nemusika <nemusika@yo.rim.or.jp>
2024-11-28 05:33:11 +0900

    Create script.js

---
+++ tcp-crypto-tls/script.js
@@ -0,0 +1,13 @@
+import tcp from "k6/x/tcp-crypto-tls";
+
+export default function () {
+  const address = "example.com:443"; // HTTPSサーバーのアドレス
+  const conn = tcp.ConnectTLS(address, true); // TLS接続
+  console.log(`Connected to ${address}`);
+
+  tcp.WriteLn(conn, "Hello, server!");
+  const response = tcp.Read(conn, 1024);
+  console.log(`Response: ${String.fromCharCode(...response)}`);
+
+  tcp.Close(conn);
+}
